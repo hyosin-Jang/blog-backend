510dd588761c3f669c885b56013465c1b044798d
MJ.CH <81309465+SUMMERLOVE7@users.noreply.github.com>
2021-07-25 22:28:46 +0900

    feat / frontend blog code upload

---
+++ client/src/page/main.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './main.css';
+
+import { Route, Link, Switch } from 'react-router-dom';
+import { Home, Write } from './index.js'; 
+import { Right_Write } from './right/index.js'; 
+
+
+
+
+class main extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div className='Mains'>
+            <div id='Mains-left'>
+                <h3> Left Side </h3>
+            </div>
+
+        <div>
+            <Route path='/' component={Home} exact/>
+            <Route path='/write' component={Write} />
+        </div>
+
+        <div id='Mains-right'>
+        <Route path='/write' component={Right_Write} />
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default main;
+
---
+++ client/src/page/right/index.js
@@ -0,0 +1,3 @@
+export { default as Right_Write } from './right_write';
+export { default as Home } from './right_write';
+export { default as Write } from './right_write';
