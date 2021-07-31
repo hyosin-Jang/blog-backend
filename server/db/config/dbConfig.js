fe242c1d3b678cf72ef2eaa34b511da3794ac6d6
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2021-07-31 17:55:04 +0900

    Update dbConfig.js

--- server/db/config/dbConfig.js
+++ server/db/config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports={
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'tjwls7',
-    database: 'blog'
+    host: '',
+    port: '',
+    user: '',
+    password: '',
+    database: ''
 };
