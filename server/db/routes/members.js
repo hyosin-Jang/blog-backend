972f8884f54d60a8e74ba71a3f695c51d10fe161
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2021-08-06 00:09:01 +0900

    Update members.js

--- server/db/routes/members.js
+++ server/db/routes/members.js
@@ -41,7 +41,7 @@ router.get("/:m_id",(req,res)=>{
 //Insert
 router.post("/",(req,res)=>{
     var sql='INSERT INTO members VALUES(?,?,?,?,?)';
-    var params=[req.body.m_id,req.body.m_email,req.body.m_verified_email,req.body.m_picture,req.body.m_hd];
+    var params=[req.body.m_tokens,req.body.m_id,req.body.m_email,req.body.m_name,req.body.m_picture];
 
     con.query(sql,params,(error,rows)=>{
         if(error){
@@ -54,7 +54,7 @@ router.post("/",(req,res)=>{
 
 //Update
 router.put("/:m_id",(req,res)=>{
-    var sql = 'UPDATE members SET m_email='+req.body.m_email+', m_picture='+req.body.m_picture+', m_hd='+req.body.m_hd+' WHERE m_id=\''+req.params.m_id+"\'";
+    var sql = 'UPDATE members SET m_email='+req.body.m_email+', m_name='+req.body.m_name+', m_picture='+req.body.m_picture+' WHERE m_id=\''+req.params.m_id+"\'";
     connection.query(sql, (error, rows)=>{
         if(error) {
             throw error;
