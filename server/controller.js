12596c8a4b8b9e48b8305265ec2bc90521881108
MJ.CH <81309465+SUMMERLOVE7@users.noreply.github.com>
2021-08-14 21:19:11 +0900

    Upload controller file

---
+++ server/controller.js
@@ -0,0 +1,78 @@
+const psth = require('path');
+const model = require('./models');
+
+/*
+const salt  = require(path.join(__dirname, 'config', 'dbConfig.js'))
+ .salt
+*/
+
+//const hashing = require(path.join(__dirname, 'config', 'hashing.js'))
+
+
+module.exports = {
+    needs: () => upload,
+
+    /*
+    api: {
+        sendPw : (req, res) =>{
+            const body = req.body;
+            //const hash = hashing.enc(body.id, body.password, salt)
+            
+            console.log(body.id, body.password)
+            
+        
+
+            //console.log('1. salt 값 : ' , salt)
+            //console.log('3. hash 결과 : ', hash)
+        },
+    },
+    */
+    add : {
+        board : (req, res) => {
+            console.log(req.body);
+            const body = req.body;
+
+            model.add.board(body, result => {
+                if(result) {
+                    res.send(true);
+                }
+            })
+
+        }
+    },
+    get : {
+        board : (req, res) => {
+  
+            const body  = req.body; 
+
+            model.get.board(body, result => {
+                if(result) {
+                res.send(result);
+                }
+            })
+        },
+
+        board_cnt : (req, res) => {
+
+            model.get.board_cnt(cnt => {
+              const result = { cnt : cnt }
+              res.send(result)
+            })
+        },
+        board_data : (req, res) => {
+            const body = req.body;
+    
+            model.get.board_data(body, data => {
+              const result = { data : data }
+              res.send(result)
+            })
+        }
+  
+    
+        
+          
+    }
+
+
+   
+}
