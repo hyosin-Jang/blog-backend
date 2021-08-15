6e21b4eb96b9d310435f2d5690ee70d3ee9ad6dd
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-15 22:18:04 +0900

    Delete client directory

--- client/src/views/inc/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from './home';
-export { default as Test } from './test';
-export { default as Head } from './header';
--- client/src/views/page/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Main } from './main';
-export { default as Home } from './home';
-export { default as Write } from './write';
-export { default as List } from './list';
-export { default as View } from './view';
