bff1821469159650191876843eb8d501e76e9b4a
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-15 21:38:27 +0900

    ✅ Connect model

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const Member = require('./members');
 const Board = require('./boards');
+const Comment = require("./comments");
+const Category = require("./categories");
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -13,12 +15,18 @@ db.sequelize = sequelize;
 // memeber 모델 연결, db에 require해서 member 접근
 db.Member = Member;
 db.Board = Board;
+db.Comment = Comment;
+db.Category = Category;
 
 // static.init 메서드 호출
 Member.init(sequelize);
 Board.init(sequelize);
+Comment.init(sequelize);
+Category.init(sequelize);
 
 Member.associate(db);
 Board.associate(db);
+Comment.associate(db);
+Category.associate(db);
 
 module.exports = db;
