34b0d2e0bc6a2d16f8fc6bf17ca55d17af0ae845
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-22 16:49:34 +0900

    Add config

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    google:{
+        clientID: '761604607478-u6t8vtsdeo7s1targd13gcnogb93gemn.apps.googleusercontent.com',
+        clientSecret: 'EN9Z0Hp1tLvM4683iYVJaiPH'
+    },
+
+};
