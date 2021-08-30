74414c577988140b4a681b2b222a72ed99e0e199
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-30 23:06:11 +0900

    update: controller.js

--- server/routers/controller.js
+++ server/routers/controller.js
@@ -1,70 +1,44 @@
 const path = require("path");
 const boards = require("./boards");
 const comments = require("./comments");
+const members = require("./members");
+
+const sequelize = require("../models").sequelize;
+const Op = require("sequelize");
+const cors = require("cors");
+
+const {
+  //Admin,
+  Board,
+  Category,
+  Member
+  //Sequelize: { Op }
+} = require("../models");
 
 module.exports = {
-  needs: () => upload,
+  //needs: () => upload,
 
-  /*
-  api: {
-      sendPw : (req, res) =>{
-          const body = req.body;
-          //const hash = hashing.enc(body.id, body.password, salt)
-          
-          console.log(body.id, body.password)
-          
-      
-          //console.log('1. salt 값 : ' , salt)
-          //console.log('3. hash 결과 : ', hash)
-      },
-  },
-  */
   add: {
-    board : (req, res) => {
-      console.log(req.body);
-      const body = req.body;
+    board: (req, res) => {
+      //{title: 'oo', contents:'oo'}
+      console.log("req", req.body);
+      const status = "성공";
+      res.send(200, status);
 
-      model.add.board(body, result => {
-          if(result) {
-              res.send(true);
-          }
-      })
-  },
-
-  board_cnt : (req, res) => {
-
-      model.add.board_cnt(cnt => {
-        const result = { cnt : cnt }
-        if(result) {
-          res.send(true);
-        }      
-      })
-  },
-
-  board_data : (req, res) => {
-      const body = req.body;
-
-      model.add.board_data(body, data => {
-        const result = { data : data }
-        if(result) {
-          res.send(true);
+      boards.add.board(req.body, result => {
+        /*
+        if (result) {
+          res.send(200, { result: true }); // => true면 게시물 추가
         }
-      })
-    }
-
-    /*
-    comment: (req, res) => {
-      const body = req.body;
-
-      comments.add.comment(body, result => {
-        res.send(result);
+        */
       });
     }
-
-    */
   },
+
+  //  { num: id }
   get: {
     board: (req, res) => {
+      console.log("req", req.body);
       const body = req.body;
 
       boards.get.board(body, result => {
@@ -73,19 +47,129 @@ module.exports = {
         }
       });
     },
-    board_data: (req, res) => {
+    /*
+    comment: (req, res) => {
       const body = req.body;
 
-      boards.get.board_data(body, data => {
-        const result = { data: data };
+      comments.add.comment(body, result => {
         res.send(result);
+      });
+    }
+    */
+
+    member: (req, res) => {
+      // req.body 전달
+      members.get.member(req.body, result => {
+        // result = "title", "date"
+        if (result) {
+          res.send(result);
+        }
+      });
+    },
+
+    pre_and_next: (body, callback) => {
+      let result = {};
+
+      let where_1 = body.category;
+      let where_2 = "";
+
+      if (!body.category) {
+        // 전체보기를 클릭했을 경우
+        where_2 = 0;
+      } else if (body.category) {
+        // 카테고리를 클릭했을 경우
+        where_2 = null;
+      }
+
+      Board.findAll({
+        // 다음글 구하기
+        where: {
+          board_id: {
+            [Op.gt]: body.b_id
+          },
+          cat_id: {
+            [Op.or]: {
+              [Op.eq]: where_1,
+              [Op.gt]: where_2
+            }
+          }
+        },
+        limit: 1
+      }).then(next => {
+        result["next"] = next;
+
+        // 이전글 구하기
+        Board.findAll({
+          where: {
+            board_id: {
+              [Op.lt]: body.b_id
+            },
+            cat_id: {
+              [Op.or]: {
+                [Op.eq]: where_1,
+                [Op.gt]: where_2
+              }
+            }
+          },
+          limit: 1,
+          order: sequelize.literal("board_id DESC")
+        }).then(pre => {
+          result["pre"] = pre;
+          callback(result);
+        });
+      });
+    },
+
+    /*
+    board_cnt: (body, callback) => {
+      let search = "node.js";
+
+      if (body.search) {
+        search = "%" + body.search + "%";
+      }
+
+      let where_1 = body.category;
+      let where_2 = "";
+
+      if (!body.category) {
+        // 전체보기를 클릭했을 경우
+        where_2 = 0;
+      } else if (body.category) {
+        // 카테고리를 클릭했을 경우
+        where_2 = null;
+      }
+
+      Board.count({
+        where: {
+          title: { [Op.like]: search },
+          contents: { [Op.like]: search },
+          cat_id: {
+            [Op.or]: {
+              [Op.eq]: where_1,
+              [Op.gt]: where_2
+            }
+          }
+        }
+      }).then(result => {
+        callback(result);
+      });
+    },
+*/
+    board_data: (req, res) => {
+      const body = req.body; // board_id
+
+      console.log("req.body:", body);
+      const num = Number(body.num);
+      console.log(num);
+      boards.get.board_data(num, data => {
+        res.send(200, data);
       });
     },
     comment: (req, res) => {
       const body = req.body;
 
       comments.get.comment(body, data => {
-        res.send(data);
+        res.send(200, data); // => 댓글 내용 전송
       });
     }
   },
@@ -95,7 +179,7 @@ module.exports = {
 
       boards.update.hit(body, result => {
         if (result) {
-          res.send(true);
+          res.send(200, { result: true });
         }
       });
     },
@@ -103,14 +187,14 @@ module.exports = {
       const body = req.body;
 
       boards.update.board(body, data => {
-        res.send(true);
+        res.send(200, { result: true });
       });
     },
     comment: (req, res) => {
       const body = req.body;
 
       comments.update.comment(body, result => {
-        res.send(true);
+        res.send(200, { result: true });
       });
     }
   },
@@ -119,14 +203,14 @@ module.exports = {
       const body = req.body;
 
       boards.delete.board(body, () => {
-        res.send(true);
+        res.send(200, { result: true });
       });
     },
     comment: (req, res) => {
       const body = req.body;
 
       comments.delete.comment(body, () => {
-        res.send(true);
+        res.send(200, { result: true });
       });
     }
   }
